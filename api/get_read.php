c9eca18d606ea80f2aeb3d212e4ca9a2cbfec482
el <eugeneleo14@gmail.com>
2024-07-05 06:42:08 +0700

    fix

--- api/get_read.php
+++ api/get_read.php
@@ -22,7 +22,7 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
         $stmt->execute();
         $manga = $stmt->fetch();
 
-        $sql = 'SELECT * FROM chapters WHERE manga_id = :manga_id ORDER BY id DESC';
+        $sql = "SELECT * FROM chapters c WHERE manga_id = :manga_id ORDER BY CAST(SUBSTRING_INDEX(c.name, ' - ', 1) AS UNSIGNED) DESC ";
         $stmt = $db->prepare($sql);
         $stmt->bindParam(':manga_id', $chapter['manga_id'], PDO::PARAM_STR);
         $stmt->execute();
