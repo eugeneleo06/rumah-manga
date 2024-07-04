05b9acb7d0d9df79793d38c1fe797d65fee4549e
eugenel <eugene.412019042@civitas.ukrida.ac.id>
2024-07-04 12:04:42 +0700

    debug

--- api/get_view.php
+++ api/get_view.php
@@ -4,7 +4,7 @@ include('config/db.php');
 
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
     try{
-        $sql = 'SELECT *, a.name as author_name FROM mangas m
+        $sql = 'SELECT *, m.id as manga_id, a.name as author_name FROM mangas m
          LEFT JOIN authors a ON a.id = m.author_id
          WHERE m.secure_id = :secure_id';
         $stmt = $db->prepare($sql);
@@ -25,11 +25,11 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
         }
         $genreNamesCombined = implode(" - ", $genreNames);
         $manga['genre'] = $genreNamesCombined;
-        var_dump($manga['id']);
+        var_dump($manga['manga_id']);
         # GET CHAPTER
         $sql = 'SELECT * FROM chapters c WHERE c.manga_id = :manga_id ORDER BY created_date ASC';
         $stmt = $db->prepare($sql);
-        $stmt->bindParam(':manga_id', $manga['id'], PDO::PARAM_STR);
+        $stmt->bindParam(':manga_id', $manga['manga_id'], PDO::PARAM_STR);
         $stmt->execute();
         $chapters = $stmt->fetchAll();
         var_dump($stmt);
