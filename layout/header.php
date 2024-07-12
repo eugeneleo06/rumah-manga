5f314149ad8ec0b75f009bccc71c0cb669c2ad17
el <eugeneleo14@gmail.com>
2024-07-12 10:29:41 +0700

    favicon

--- layout/header.php
+++ layout/header.php
@@ -2,8 +2,28 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Rumah Manga</title>
+    <base href="">
+    <meta charset="utf-8" />
+    <title>RUMAHMANGA - Baca Manga Online Bahasa Indonesia Gratis</title>
+    <meta http-equiv="Accept-CH" content="Sec-CH-UA-Platform-Version, Sec-CH-UA-Model" />
+    <link rel="icon" type="image/x-icon" href="img/favicon-rumahmanga.ico" />
+    <link rel="amphtml" href="https://rumahmanga.com/">
+    <link rel="canonical" href="https://rumahmanga.com/" />
+    <meta property="og:site_name" content="https://rumahmanga.com/" />
+    <meta property="og:title" content="RUMAHMANGA - Baca Manga Online Bahasa Indonesia Gratis" />
+    <meta property="og:url" content="https://rumahmanga.com/" />
+    <meta property="og:type" content="product" />
+    <meta property="og:description" content="RUMAHMANGA adalah Link Login Alternatif Slot Online Now yang sudah pasti dijamin kemenangan maxwin setiap kali anda memutar spin permainan slot anti rungkad terrsebut" />
+    <meta property="og:image" content="https://pub-2bfa6b528bf54fa9a840c5feca5a3a76.r2.dev/img/logo-gif.gif" />
+    <meta itemprop="name" content="RUMAHMANGA - Baca Manga Online Bahasa Indonesia Gratis" />
+    <meta itemprop="url" content="https://rumahmanga.com/" />
+    <meta itemprop="description" content="RUMAHMANGA adalah Link Login Alternatif Slot Online Now yang sudah pasti dijamin kemenangan maxwin setiap kali anda memutar spin permainan slot anti rungkad terrsebut" />
+    <meta itemprop="thumbnailUrl" content="http://static1.squarespace.com/static/65ae3107557c3e3e731125c1/65ae31874ee76a4e35e6df19/65b9fb0b6f12e76de73208be/1706853953515/banner+amp.png?format=1500w" />
+    <link rel="image_src" href="http://static1.squarespace.com/static/65ae3107557c3e3e731125c1/65ae31874ee76a4e35e6df19/65b9fb0b6f12e76de73208be/1706853953515/banner+amp.png?format=1500w" />
+    <meta itemprop="image" content="http://static1.squarespace.com/static/65ae3107557c3e3e731125c1/65ae31874ee76a4e35e6df19/65b9fb0b6f12e76de73208be/1706853953515/banner+amp.png?format=1500w" />
+    <meta name="description" content="RUMAHMANGA adalah tempat baca manga online bahasa indonesia yang gratis selamanya untuk kamu para anime dan manga lovers bisa enjoy dan baca manga online favorit kamu disini ya !" />
     <link href="lib/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet" type="text/css">
     <link href="css/custom.css" rel="stylesheet" type="text/css">
